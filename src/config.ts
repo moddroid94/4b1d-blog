6e94008a64a63c3f100f90e9c1d55aa82eaadfc6
moddroid94 <samuel.brizzi94@gmail.com>
2026-09-06 21:59:28 +0200

    updated blog settings and author info

--- src/config.ts
+++ src/config.ts
@@ -59,7 +59,7 @@ export const profileConfig: ProfileConfig = {
 	links: [
 		{
 			name: "Printables",
-			icon: "fa6-brands:printables", // Visit https://icones.js.org/ for icon codes
+			icon: "selfhst:printables", // Visit https://icones.js.org/ for icon codes
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
 			url: "https://www.printables.com/@n0d4y",
